9b053a279f036833eb4fdae23de9e1c018b42c00
mhtran <tranmanhhoang@gmail.com>
2017-01-11 10:24:00 +0700

    reset

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -12,22 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let path = Bundle.main.path(forResource: "data", ofType: "json") {
-        do {
-            
-            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
-            let jsonObj = JSON(data: data)
-            if jsonObj != JSON.null {
-                print("jsonData:\(jsonObj)")
-            } else {
-                print("Could not get json from file, make sure that file contains valid json.")
-            }
-        } catch let error {
-            print(error.localizedDescription)
-            }
-        } else {
-            print("Invalid filename/path.")
-        }
+        
     }
     
     
